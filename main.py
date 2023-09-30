9b0f8d6f64949252231849becb890424fdd37ca8
Tamim Miah <75170424+TamGamer97@users.noreply.github.com>
2023-09-30 15:19:47 +0100

    Update main.py

--- main.py
+++ main.py
@@ -2,9 +2,21 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
+import asyncio
+
+from pywizlight import wizlight, PilotBuilder, discovery
+
+
 @app.route('/')
 def index():
     return 'heloo'
+
+@app.route('/ChangeClr')
+async def ChangeClr():
+    light = wizlight("192.168.1.41", port=38899)
+    await light.turn_on(PilotBuilder())
+    await light.turn_on(PilotBuilder(rgb = (255, 000, 000)))
 
 
 if __name__ == '__main__':
