99aa767d9bc7f7103e55e75e1c7cd5c98b74c8bf
Tamim Miah <75170424+TamGamer97@users.noreply.github.com>
2023-09-30 15:38:51 +0100

    Update main.py

--- main.py
+++ main.py
@@ -14,17 +14,10 @@ def index():
 
 @app.route('/ChangeClr')
 async def ChangeClr():
-    # light = wizlight("192.168.1.41", port=38899)
-    # await light.turn_on(PilotBuilder(rgb = (255, 000, 000)))
-    # print('Done')
-    # return 'Light Changed to red'
-
-    bulbs = await discovery.discover_lights(broadcast_space="192.168.1.255")
-    print("Bulb IP address & Port: "+ str(bulbs[0].ip) + " " + str(bulbs[0].port))
-    for bulb in bulbs:
-        print('Turning bulb off')
-        # Turn off all available bulbs
-        await bulb.turn_off()
+    light = wizlight("192.168.1.41", port=38899)
+    await light.turn_on(PilotBuilder(rgb = (255, 000, 000)))
+    print('Done')
+    return 'Light Changed to red'
 
 
 if __name__ == '__main__':
